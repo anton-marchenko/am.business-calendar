9f3b0665d0cdb2eb587b27dd9aa8b4c930591880
anton-marchenko <fattan@ya.ru>
2019-10-01 08:37:20 +0300

    test(FileUploadService): File reading test has been added

--- projects/business-calendar/src/test/file-upload.service.spec.ts
+++ projects/business-calendar/src/test/file-upload.service.spec.ts
@@ -8,4 +8,12 @@ describe('FileUploadService', () => {
     const service: FileUploadService = TestBed.get(FileUploadService);
     expect(service).toBeTruthy();
   });
+
+  it('should file be read', () => {
+    const service: FileUploadService = TestBed.get(FileUploadService);
+    const fileContent = 'test';
+    const fakeFile = new File([fileContent], 'filename', {type: 'text/csv'});
+
+    service.upload(fakeFile).then(result => expect(result).toBe(fileContent));
+  });
 });
