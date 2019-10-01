ac91cf228c02c9b6dc768a2da96dc32347a6c7a6
anton-marchenko <fattan@ya.ru>
2019-10-01 07:47:24 +0300

    Storybook config and global styles

---
+++ .storybook/config.js
@@ -0,0 +1,5 @@
+import { configure } from '@storybook/angular';
+import '!style-loader!css-loader!sass-loader!../src/stories/styles.scss';
+
+// automatically import all files ending in *.stories.ts
+configure(require.context('../src/stories', true, /\.stories\.ts$/), module);
